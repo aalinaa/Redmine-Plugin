497aabd95794d14e44e595513b3331cc105dc0c9
Alina_Gutsul <alina.gutsul@tul.cz>
2016-08-16 21:34:48 +0200

    tests

--- test/functional/add_author_controller_test.rb
+++ test/functional/add_author_controller_test.rb
@@ -1,8 +1,31 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-class AddAuthorControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+class AddAuthorControllerTest < ActionController::TestCase 
+  setup do
+    @user = User.first
+    @project = Project.first
+  end
+
+  test 'should get index' do
+    get :index, project_id: @project.id
+    assert_response :success
+    assert_template 'index'
+  end
+
+  test 'should get add (create new author and redirect to index)' do
+    Author.delete_all
+    get :add, project_id: @project.id, users: @user.id
+    author = Author.first
+    assert_equal author.project_id, @project.id
+    assert_equal author.user_id, @user.id
+    assert_response :redirect
+  end
+
+  test 'should get delete (delete author and redirect to index)' do
+    author = Author.create(project_id: @project.id, user_id: @user.id)
+    get :delete, project_id: @project.id, id: author.id
+    author = Author.where(id: author.id)
+    assert_equal author.empty?, true
+    assert_response :redirect
   end
 end
--- test/unit/author_test.rb
+++ test/unit/author_test.rb
@@ -1,9 +1,29 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class AuthorTest < ActiveSupport::TestCase
+  setup do
+    @user1 = User.first
+    @user2 = User.second
+    @user3 = User.third
+    @project = Project.first
+  end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  test 'should not add tha same author twice' do
+    Author.delete_all
+    Author.create(project_id: @project.id, user_id: @user1.id)
+    authors = Author.all
+    assert_equal authors.size, 1
+    Author.create(project_id: @project.id, user_id: @user1.id)
+    authors = Author.all
+    assert_equal authors.size, 1
+  end
+
+  test 'lala' do
+    Author.delete_all
+    Author.create(project_id: @project.id, user_id: @user1.id)
+    Author.create(project_id: @project.id, user_id: @user2.id)
+    Author.create(project_id: @project.id, user_id: @user3.id)
+    authors_from_project = Author.all_from_project(@project.id)    
+    assert_equal authors_from_project.size, 3
   end
 end
